f691f8959ce777a59c23cfd5d8435d57aab0e3bd
Marcelo <marcelocanhoto10@hotmail.com>
2020-08-31 19:36:32 -0300

    Initial Commit

---
+++ smartplace/Config/Database/Migrations/20200831222532_UserMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace smartplace.Config.Database.Migrations
+{
+    public partial class UserMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ smartplace/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smartplace.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set;}
+    }
+
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().HasKey(m => m.Id);
+            base.OnModelCreating(builder);
+        }
+    }
+}
